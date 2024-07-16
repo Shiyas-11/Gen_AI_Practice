0f75b354f2c258dcd5547b91f1478a4ad3597778
Shiyas <shiyas11official@gmail.com>
2024-07-16 17:00:45 +0530

    Finished ReviewSystem Using Streamlit(Ascend-1)

--- ReviewSystem/streamlit_work.py
+++ ReviewSystem/streamlit_work.py
@@ -1,7 +1,7 @@
 from Analyzer import Analyzer
 import streamlit as st
 from ReviewSystem import Email,Review
-from json import dump
+from json import dump,load
 
 st.header("Review Bot")
 st.write("YOUR AI ASSIST-BOT")
@@ -13,7 +13,8 @@ email=st.text_input("INPUT EMAIL")
 st.caption("Review")
 rev=st.text_area("INPUT REVIEW")
 button=st.button("Submit")
-
+rev=st.file_uploader("Upload JSON file of reviews here :",type="json")
+button2=st.button("Submit Json")
 AI_sorted={}
 Emails={}
 if button:
@@ -24,6 +25,16 @@ if button:
 		st.caption("Rating")
 		st.write(out["Rating"]["review"])
 		st.write(out["Mail"][email])
+elif button2:
+	rev=load(rev)
+	for r in rev:
+		out=Analyzer(name,email,rev,Email,Review)
+		AI_sorted[email]=out["Rating"]
+		Emails[email]=out["Mail"]
+		st.caption("Rating")
+		# st.write(out["Rating"]["review"])
+		# st.write(out["Mail"][email])
+	st.success("THE REVIEWS HAVE BEEN ANALYZED AND STORED THE EMAIL RESULSTS SUCCESSFULLY")
 
 with open("mails.json","a") as E:
 	dump(Emails,E,indent=4)
